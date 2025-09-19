71229b1b9064124c6a4d7537adeb33399664c235
Alex-MarquezC <aO1O27723@tec.mx>
2025-09-19 00:53:41 -0600

    Se definieron los parametros de directions para arreglar el erro donde no los detectaba

--- snake.py
+++ snake.py
@@ -18,6 +18,12 @@ food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
 
+directions = [
+    vector(10, 0),
+    vector(-10, 0),
+    vector(0, 10),
+    vector(0, -10)
+]
 
 def change(x, y):
     """Change snake direction."""
